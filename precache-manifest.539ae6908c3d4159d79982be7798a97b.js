18786b4c4290368109b7bed5db8db25ca22c1bbb
Dillon M <dillon32300@gmail.com>
2022-09-04 19:10:09 -0500

    Updates

---
+++ precache-manifest.539ae6908c3d4159d79982be7798a97b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9b32a2764ad118dffc",
+    "url": "/waldo/static/css/main.413e9c22.chunk.css"
+  },
+  {
+    "revision": "dc9b32a2764ad118dffc",
+    "url": "/waldo/static/js/main.dc9b32a2.chunk.js"
+  },
+  {
+    "revision": "e7a3fba3654bdcb8adc6",
+    "url": "/waldo/static/js/1.e7a3fba3.chunk.js"
+  },
+  {
+    "revision": "eeef8028351781b9aa64",
+    "url": "/waldo/static/js/runtime~main.eeef8028.js"
+  },
+  {
+    "revision": "953dadcb795afefb2c6b3083b99ca1f5",
+    "url": "/waldo/static/media/waldo.953dadcb.jpg"
+  },
+  {
+    "revision": "2b46d42cfc6070d3a41776bc2227f34b",
+    "url": "/waldo/static/media/mario.2b46d42c.png"
+  },
+  {
+    "revision": "1de5f70d8d1ba7df4a1ec8cd63614130",
+    "url": "/waldo/static/media/fox.1de5f70d.png"
+  },
+  {
+    "revision": "b6af7485c4cfd03529a69e46a6b080c9",
+    "url": "/waldo/static/media/star.b6af7485.png"
+  },
+  {
+    "revision": "994e3fff2e1229693ea9180fcb99c1df",
+    "url": "/waldo/index.html"
+  }
+];
